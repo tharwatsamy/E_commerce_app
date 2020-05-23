e24760d33c9f87d7dab5cae3fe2e2b0256def6ce
Tharwat samy <tharwatsamy2004@gmail.com>
2020-05-23 18:34:23 +0200

    commit

--- lib/screens/user/CartScreen.dart
+++ lib/screens/user/CartScreen.dart
@@ -1,6 +1,11 @@
+import 'dart:ui';
+
+import 'package:buy_it/constants.dart';
 import 'package:buy_it/models/product.dart';
 import 'package:buy_it/provider/cartItem.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/painting.dart';
 import 'package:provider/provider.dart';
 
 class CartScreen extends StatelessWidget {
@@ -8,17 +13,103 @@ class CartScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     List<Product> products = Provider.of<CartItem>(context).products;
+    final double screenHeight = MediaQuery.of(context).size.height;
+    final double screenWidth = MediaQuery.of(context).size.width;
     return Scaffold(
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Column(
-            children: <Widget>[
-              Text(products[index].pName),
-              Text(products[index].pQuantity.toString()),
-            ],
-          );
-        },
-        itemCount: products.length,
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        leading: GestureDetector(
+          onTap: () {
+            Navigator.pop(context);
+          },
+          child: Icon(
+            Icons.arrow_back,
+            color: Colors.black,
+          ),
+        ),
+        title: Text(
+          'My Cart',
+          style: TextStyle(color: Colors.black),
+        ),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: ListView.builder(
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Container(
+                    height: screenHeight * .15,
+                    child: Row(
+                      children: <Widget>[
+                        CircleAvatar(
+                          radius: screenHeight * .15 / 2,
+                          backgroundImage:
+                              AssetImage(products[index].pLocation),
+                        ),
+                        Expanded(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Padding(
+                                padding: const EdgeInsets.only(left: 10),
+                                child: Column(
+                                  mainAxisAlignment: MainAxisAlignment.center,
+                                  children: <Widget>[
+                                    Text(
+                                      products[index].pName,
+                                      style: TextStyle(
+                                          fontSize: 18,
+                                          fontWeight: FontWeight.bold),
+                                    ),
+                                    SizedBox(
+                                      height: 10,
+                                    ),
+                                    Text(
+                                      '\$ ${products[index].pPrice}',
+                                      style: TextStyle(
+                                          fontWeight: FontWeight.bold),
+                                    )
+                                  ],
+                                ),
+                              ),
+                              Padding(
+                                padding: const EdgeInsets.only(right: 20),
+                                child: Text(
+                                  products[index].pPrice.toString(),
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                              )
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    color: kSecondaryColor,
+                  ),
+                );
+              },
+              itemCount: products.length,
+            ),
+          ),
+          ButtonTheme(
+            minWidth: screenWidth,
+            height: screenHeight * .08,
+            child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(10),
+                      topLeft: Radius.circular(10))),
+              onPressed: () {},
+              child: Text('Order'.toUpperCase()),
+              color: kMainColor,
+            ),
+          )
+        ],
       ),
     );
   }
