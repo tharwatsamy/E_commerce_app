332170bbd374010f27a04c01af8080741602bf81
Tharwat samy <tharwatsamy2004@gmail.com>
2020-05-24 07:49:11 +0200

    commit

--- lib/screens/user/CartScreen.dart
+++ lib/screens/user/CartScreen.dart
@@ -15,87 +15,100 @@ class CartScreen extends StatelessWidget {
     List<Product> products = Provider.of<CartItem>(context).products;
     final double screenHeight = MediaQuery.of(context).size.height;
     final double screenWidth = MediaQuery.of(context).size.width;
+    final double appBarHeight = AppBar().preferredSize.height;
+    final double statusBarHeight = MediaQuery.of(context).padding.top;
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.white,
         elevation: 0,
-        leading: GestureDetector(
-          onTap: () {
-            Navigator.pop(context);
-          },
-          child: Icon(
-            Icons.arrow_back,
-            color: Colors.black,
-          ),
-        ),
         title: Text(
           'My Cart',
           style: TextStyle(color: Colors.black),
         ),
+        leading: Icon(
+          Icons.arrow_back,
+          color: Colors.black,
+        ),
       ),
       body: Column(
         children: <Widget>[
-          Expanded(
-            child: ListView.builder(
-              itemBuilder: (context, index) {
-                return Padding(
-                  padding: const EdgeInsets.all(15),
-                  child: Container(
-                    height: screenHeight * .15,
-                    child: Row(
-                      children: <Widget>[
-                        CircleAvatar(
-                          radius: screenHeight * .15 / 2,
-                          backgroundImage:
-                              AssetImage(products[index].pLocation),
-                        ),
-                        Expanded(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                            children: <Widget>[
-                              Padding(
-                                padding: const EdgeInsets.only(left: 10),
-                                child: Column(
-                                  mainAxisAlignment: MainAxisAlignment.center,
-                                  children: <Widget>[
-                                    Text(
-                                      products[index].pName,
+          LayoutBuilder(builder: (context, constrains) {
+            if (products.isNotEmpty) {
+              return Expanded(
+                child: ListView.builder(
+                  itemBuilder: (context, index) {
+                    return Padding(
+                      padding: const EdgeInsets.all(15),
+                      child: Container(
+                        height: screenHeight * .15,
+                        child: Row(
+                          children: <Widget>[
+                            CircleAvatar(
+                              radius: screenHeight * .15 / 2,
+                              backgroundImage:
+                                  AssetImage(products[index].pLocation),
+                            ),
+                            Expanded(
+                              child: Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Padding(
+                                    padding: const EdgeInsets.only(left: 10),
+                                    child: Column(
+                                      mainAxisAlignment:
+                                          MainAxisAlignment.center,
+                                      children: <Widget>[
+                                        Text(
+                                          products[index].pName,
+                                          style: TextStyle(
+                                              fontSize: 18,
+                                              fontWeight: FontWeight.bold),
+                                        ),
+                                        SizedBox(
+                                          height: 10,
+                                        ),
+                                        Text(
+                                          '\$ ${products[index].pPrice}',
+                                          style: TextStyle(
+                                              fontWeight: FontWeight.bold),
+                                        )
+                                      ],
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.only(right: 20),
+                                    child: Text(
+                                      products[index].pQuantity.toString(),
                                       style: TextStyle(
-                                          fontSize: 18,
+                                          fontSize: 16,
                                           fontWeight: FontWeight.bold),
                                     ),
-                                    SizedBox(
-                                      height: 10,
-                                    ),
-                                    Text(
-                                      '\$ ${products[index].pPrice}',
-                                      style: TextStyle(
-                                          fontWeight: FontWeight.bold),
-                                    )
-                                  ],
-                                ),
+                                  )
+                                ],
                               ),
-                              Padding(
-                                padding: const EdgeInsets.only(right: 20),
-                                child: Text(
-                                  products[index].pPrice.toString(),
-                                  style: TextStyle(
-                                      fontSize: 16,
-                                      fontWeight: FontWeight.bold),
-                                ),
-                              )
-                            ],
-                          ),
-                        )
-                      ],
-                    ),
-                    color: kSecondaryColor,
-                  ),
-                );
-              },
-              itemCount: products.length,
-            ),
-          ),
+                            )
+                          ],
+                        ),
+                        color: kSecondaryColor,
+                      ),
+                    );
+                  },
+                  itemCount: products.length,
+                ),
+              );
+            } else {
+              return Container(
+                height: screenHeight -
+                    (screenHeight * .08) -
+                    appBarHeight -
+                    statusBarHeight,
+                child: Center(
+                  child: Text('Cart is Empty'),
+                ),
+              );
+            }
+          }),
           ButtonTheme(
             minWidth: screenWidth,
             height: screenHeight * .08,
